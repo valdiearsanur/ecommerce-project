bd3920e1b3f52474b8d2fded39a781796b8f8cf7
Muhammad Valdie Arsanur <valdiearsanur@gmail.com>
2016-12-06 10:13:04 +0700

    first commit

---
+++ application/language/english/en_admin_lang.php
@@ -0,0 +1,25 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+// General
+$lang['system_system_name'] = "Shop";
+// nav
+$lang['nav_cart_count'] = "Cart ";
+$lang['nav_home'] = "Home";
+// index()
+$lang['index_all_categories'] = "All categories";
+$lang['index_add_to_cart'] = "Add to cart";
+// display_cart()
+$lang['display_cart_proceed_to_checkout'] = "Proceed to checkout";
+$lang['display_cart_quantity'] = "Quantity";
+$lang['display_cart_description'] = "Description";
+$lang['display_cart_item_price'] = "Item Price";
+$lang['display_cart_sub_total'] = "Sub-Total";
+$lang['display_cart_update_cart'] = "Update Cart";
+// user_details()
+$lang['user_details_placeholder_first_name'] = "First Name";
+$lang['user_details_placeholder_last_name'] = "Last Name";
+$lang['user_details_placeholder_email'] = "Email";
+$lang['user_details_placeholder_email_confirm'] = "Confirm Email";
+$lang['user_details_placeholder_payment_address'] = "Payment Address";
+$lang['user_details_placeholder_delivery_address'] = "Delivery Address";
+$lang['user_details_save_success'] = "Order and Customer saved to DB";
+$lang['user_details_save_error'] = "Could not save to DB";
